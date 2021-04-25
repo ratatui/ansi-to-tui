43b15627111b8742749acf73232a18378423c05a
Uttarayan Mondal <uttarayan21@gmail.com>
2021-04-25 14:08:35 +0530

    Change from ansitui to ansi-to-tui

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use std::io::Read;
 
 use ansi::ansi_to_text;
 pub fn main() {
-    let mut file = std::fs::File::open("log").unwrap();
-    // let mut file = std::fs::File::open("archlinux.ascii").unwrap();
+    // let mut file = std::fs::File::open("log").unwrap();
+    let mut file = std::fs::File::open("archlinux.ascii").unwrap();
     let mut buffer: Vec<u8> = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
     let _text = ansi_to_text(buffer);
