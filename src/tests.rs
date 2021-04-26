39f69a24b139eb226cabc484ea61a28d86cea1e6
Uttarayan Mondal <uttarayan21@gmail.com>
2021-04-26 13:46:58 +0530

    Add additional tests

--- src/tests.rs
+++ src/tests.rs
@@ -14,21 +14,30 @@ fn test_color() {
 
 #[test]
 fn test_bytes() {
-    // use crate::ansi_to_text;
-    // let bytes = vec![27_u8, 91, 51, 49, 109, 65, 65, 65];
-    // println!("{:#?}", ansi_to_text(&bytes))
+    use crate::ansi_to_text;
+    let bytes = vec![27_u8, 91, 51, 49, 109, 65, 65, 65];
+    println!("{:#?}", ansi_to_text(&bytes))
 }
 
+#[test]
+fn archlinux_ascii() {
+    use crate::ansi_to_text;
+    use std::{fs::File, io::Read};
+    let mut ascii = File::open("tests/archlinux.ascii").unwrap();
+    let mut buffer: Vec<u8> = Vec::new();
+    ascii.read_to_end(&mut buffer).unwrap();
+    let text = ansi_to_text(&buffer).unwrap();
+    for line in text.lines {
+        println!("{:?}", line.width());
+    }
+}
 #[test]
 fn ascii_test() {
     use crate::ansi_to_text;
     use std::{fs::File, io::Read};
-    let mut ascii = File::open("archlinux.ascii").unwrap();
+    let mut ascii = File::open("tests/ascii").unwrap();
     let mut buffer: Vec<u8> = Vec::new();
     ascii.read_to_end(&mut buffer).unwrap();
     let text = ansi_to_text(&buffer).unwrap();
-    // println!("lines {:?}", text.lines.len());
-    for line in text.lines {
-        println!("{:?}", line.width());
-    }
+    println!("lines {:#?}", text);
 }
