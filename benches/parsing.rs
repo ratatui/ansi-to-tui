eb26a5c0715e7fd8dbdadf0ad9c850a75308e233
servius <servius@darksailor.dev>
2026-04-06 23:07:06 +0530

    feat: Added hyperlink benchmark

--- benches/parsing.rs
+++ benches/parsing.rs
@@ -20,6 +20,17 @@ fn criterion_benchmark(c: &mut Criterion) {
             black_box(s.into_text()).unwrap();
         })
     });
+
+    const HYPERLINK: &[u8] = include_bytes!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/ascii/hyperlink.ascii"
+    ));
+    c.bench_function("Parsing hyperlink code", |b| {
+        b.iter(|| {
+            let s = black_box(&HYPERLINK);
+            black_box(s.into_text()).unwrap();
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
